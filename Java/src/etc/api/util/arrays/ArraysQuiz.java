1d32209f6a0a5b994de13791144078c4f3b2d65a
flatcalm <a_abnc@naver.com>
2023-03-13 18:25:21 +0900

    Day16

--- Java/src/etc/api/util/arrays/ArraysQuiz.java
+++ Java/src/etc/api/util/arrays/ArraysQuiz.java
@@ -1,5 +1,7 @@
 package etc.api.util.arrays;
 
+import java.util.Arrays;
+
 public class ArraysQuiz {
 	
 	public static String solution(String[] participant, String[] completion) {
@@ -10,6 +12,22 @@ public class ArraysQuiz {
         완주하지 못한 선수의 이름을 return하는 solution 함수를 완성하세요.
         완주하지 못한 자는 1명이라고 가정합니다.
         */
+		
+		// 강사님이랑 작성한 것
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		// 정렬 후 중복이 아닌 부분을 찾는 포문
+		for(int i=0; i<completion.length; i++) {
+			if(!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		
+		// for문을 다 돌았는데 리턴값이 없다면 마지막 인덱스가 중복이 아님
+		return participant[participant.length-1];
+		
+		/* 내가 한거
 		int idx = 0;
 		
 		for(int i=0; i<completion.length; i++) {
@@ -25,6 +43,7 @@ public class ArraysQuiz {
 			}
 		}
 		return participant[idx];
+		*/
 	}
 	
 	public static void main(String[] args) {
@@ -32,7 +51,7 @@ public class ArraysQuiz {
 		String[] participant = {"홍길동", "김길동", "김철수", "최철수", "박영희"};
 		String[] completion = {"홍길동", "김길동", "박영희", "김철수"};
 		
-		System.out.println(solution(participant, completion));
+		System.out.println("완주하지 못한 자 : " + solution(participant, completion));
 		
 	}
 
